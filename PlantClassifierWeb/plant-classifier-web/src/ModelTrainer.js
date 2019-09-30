fe4c2e7fa5fe0a00118dd4b60e93bc5197f8c2c9
Rebecca Deprey <rdeprey@gmail.com>
2019-09-29 21:12:07 -0400

    Adding custom machine learning model.

---
+++ PlantClassifierWeb/plant-classifier-web/src/ModelTrainer.js
@@ -0,0 +1,278 @@
+import * as tf from '@tensorflow/tfjs';
+import image1 from './roaming-gnome-data/train/66666031_429005191161791_4059693116689707642_n.jpg';
+import image2 from './roaming-gnome-data/train/66951138_709752599466100_8452059073074455381_n.jpg';
+import image3 from './roaming-gnome-data/train/67067341_141211713763598_4163311221416932011_n.jpg';
+import image4 from './roaming-gnome-data/train/67169558_167122074450639_1419495025748381850_n.jpg';
+import image5 from './roaming-gnome-data/train/67791123_502440850544776_6881530551811977080_n.jpg';
+import image6 from './roaming-gnome-data/train/67833790_506825270135214_4529723587407201164_n.jpg';
+import image7 from './roaming-gnome-data/train/70182562_133966837922902_8976080069302847050_n.jpg';
+import image8 from './roaming-gnome-data/train/70677073_2218255548285324_2585962131801263733_n.jpg';
+import image9 from './roaming-gnome-data/train/71169801_752387091859117_7643508668502813209_n.jpg';
+import image10 from './roaming-gnome-data/train/4yRcS2Zo.png';
+import image11 from './roaming-gnome-data/train/waldo2.png';
+import image12 from './roaming-gnome-data/train/waldo3.jpeg';
+import image13 from './roaming-gnome-data/train/waldo4.png';
+import image14 from './roaming-gnome-data/train/waldo5.jpeg';
+// import blackImage from './roaming-gnome-data/train/black.jpg';
+// import whiteImage from './roaming-gnome-data/train/white.jpg';
+
+const images = [
+    // blackImage,
+    // whiteImage,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+];
+
+const labels = [
+    // 'black',
+    // 'white',
+    'roaming gnome',
+    'roaming gnome',
+    'roaming gnome',
+    'roaming gnome',
+    'roaming gnome',
+    'roaming gnome',
+    'roaming gnome',
+    'roaming gnome',
+    'roaming gnome',
+    'waldo',
+    'waldo',
+    'waldo',
+    'waldo',
+    'waldo',
+];
+
+tf.setBackend('webgl');
+
+const loadImage = (src) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = typeof src === 'string' ? src : src.toDataURL("image/png");
+        img.width = 256;
+        img.height = 256;
+
+        // Returns a 3d Tensor with shape [width, height, color_channels]
+        img.onload = () => resolve(tf.browser.fromPixels(img));
+        img.onerror = (err) => reject(err);
+    });
+};
+
+// Standard CNN architectures expect images to be of a fixed size
+// Generally best to use square images
+// MobileNet expects 224x224 square images
+// This function chops off the edges of the longer side
+const cropImage = (img) => {
+    const width = img.shape[0];
+    const height = img.shape[1];
+
+    // use the shorter side as the side to which to crop
+    const shorterSide = Math.min(img.shape[0], img.shape[1]);
+    
+    // calculate beginning and ending crop points
+    const startingHeight = (height - shorterSide) / 2;
+    const startingWidth = (width - shorterSide) / 2;
+    const endingHeight = startingHeight + shorterSide;
+    const endingWidth = startingWidth + shorterSide;
+  
+    // return image data cropped to those points
+    return img.slice([startingWidth, startingHeight, 0], [endingWidth, endingHeight, 3]);
+};
+
+// Resize the now-square images to 224x224
+const resizeImage = (img) => {
+    return tf.image.resizeBilinear(img, [224, 224]);
+};
+
+// Translate the Tensor
+// Model expects input in shape of [numTrainingExamples (any), 224, 224, 3]
+// Pixel data should be a floating point number between -1 and 1 (done through normalization)
+const batchImage = (img) => {
+    // Expand our tensor to have an additional dimension, whose size is 1
+    const batchedImage = img.expandDims(0);
+
+    // Turn pixel data into a float between -1 and 1.
+    return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+}
+
+const loadAndProcessImage = (img) => {
+    const croppedImage = cropImage(img);
+    const resizedImage = resizeImage(croppedImage);
+    const batchedImage = batchImage(resizedImage);
+    return batchedImage;
+}
+
+const loadMobileNet = () => {
+    return tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+};
+
+// Get a pretrained model that includes everything up to the conv_pw_13_relu activation layer
+// because this is the last activation layer before softmax, which is activation layer used to
+// narrow the predictions to one of a thousand categories.
+function buildPretrainedModel() {
+    return loadMobileNet().then(mobilenet => {
+        const layer = mobilenet.getLayer('conv_pw_13_relu');
+        return tf.model({
+            inputs: mobilenet.inputs,
+            outputs: layer.output,
+        });
+    }).catch(err => {
+        console.log('pretrained model error: ', err);
+    });
+}
+
+// Loops through an array of images
+function loadImages(images, pretrainedModel) {
+    let promise = Promise.resolve();
+    for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+        promise = promise.then(data => {
+            return loadImage(image).then(loadedImage => {
+                // Note the use of `tf.tidy` and `.dispose()`. These are two memory management
+                // functions that Tensorflow.js exposes.
+                // https://js.tensorflow.org/tutorials/core-concepts.html
+                //
+                // Handling memory management is crucial for building a performant machine learning
+                // model in a browser.
+                return tf.tidy(() => {
+                    const processedImage = loadAndProcessImage(loadedImage);
+                    const prediction = pretrainedModel.predict(processedImage);
+
+                    if (data) {
+                        const newData = data.concat(prediction);
+                        data.dispose();
+                        return newData;
+                    }
+
+                    return tf.keep(prediction);
+                });
+            });
+        }).catch(err => {
+            console.log('promise error: ', err);
+        });
+    }
+
+    return promise;
+};
+
+const oneHot = (labelIndex, classLength) => {
+    return tf.tidy(() => tf.oneHot(tf.tensor1d([labelIndex]).toInt(), classLength));
+};
+
+const getLabelsAsObject = (labels) => {
+    let labelObject = {};
+    for (let i = 0; i < labels.length; i++) {
+      const label = labels[i];
+      if (labelObject[label] === undefined) {
+        // only assign it if we haven't seen it before
+        labelObject[label] = Object.keys(labelObject).length;
+      }
+    }
+    return labelObject;
+};
+
+const addLabels = (labels) => {
+    return tf.tidy(() => {
+        const classes = getLabelsAsObject(labels);
+        const classLength = Object.keys(classes).length;
+
+        let ys;
+        for (let i = 0; i < labels.length; i++) {
+            const label = labels[i];
+            const labelIndex = classes[label];
+            const y = oneHot(labelIndex, classLength);
+            if (i === 0) {
+                ys = y;
+            } else {
+                ys = ys.concat(y, 0);
+            }
+        }
+        return ys;
+    });
+};
+
+const getModel = (numberOfClasses) => {
+    const model = tf.sequential({
+      layers: [
+        tf.layers.flatten({inputShape: [7, 7, 256]}),
+        tf.layers.dense({
+          units: 100,
+          activation: 'relu',
+          kernelInitializer: 'varianceScaling',
+          useBias: true
+        }),
+        tf.layers.dense({
+          units: numberOfClasses,
+          kernelInitializer: 'varianceScaling',
+          useBias: false,
+          activation: 'softmax'
+        })
+      ],
+    });
+  
+    model.compile({
+      optimizer: tf.train.adam(0.0001),
+      loss: 'categoricalCrossentropy',
+      metrics: ['accuracy'],
+    });
+  
+    return model;
+}
+
+const makePrediction = (pretrainedModel, model, image) => {
+    return loadImage(image).then(loadedImage => {
+      return loadAndProcessImage(loadedImage);
+    }).then(loadedImage => {
+      const activatedImage = pretrainedModel.predict(loadedImage);
+      loadedImage.dispose();
+      return activatedImage;
+    }).then(async activatedImage => {
+      const prediction = model.predict(activatedImage);
+      const mostLikelyValue = prediction.as1D().argMax();
+      const predictionLabel = (await mostLikelyValue.data())[0];
+
+      console.log(prediction);
+      console.log(predictionLabel);
+
+      return {
+          prediction: prediction,
+          predictionLabel: predictionLabel,
+      };
+    }).catch(err => {
+        console.log('err: ', err);
+    });
+  }
+
+export const newModel = async () => {
+    return buildPretrainedModel().then(pretrainedModel => {
+        return loadImages(images, pretrainedModel).then(xs => {
+            const ys = addLabels(labels);
+            const model = getModel(2);
+
+            model.fit(xs, ys, {
+                epochs: 20,
+                shuffle: true,
+            });
+
+            return {
+                pretrainedModel: pretrainedModel,
+                model: model,
+            };
+        });
+    });
+};
+
+export const predictWithNewModel = async (newModel, image) => {
+    return await makePrediction(newModel.pretrainedModel, newModel.model, image);
+}
--- PlantClassifierWeb/plant-classifier-web/src/Predictions.jsx
+++ PlantClassifierWeb/plant-classifier-web/src/Predictions.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Predictions.scss';
-import { loadMobileNet, classifyImage } from './mobileNet';
+// import { loadMobileNet, classifyImage } from './mobileNet';
+import { newModel, predictWithNewModel } from './ModelTrainer';
 import kebabCase from 'lodash/kebabCase';
 import classNames from 'classnames';
 
@@ -19,12 +20,14 @@ export default function Predictions(props) {
             
             async function getPredictions() {
                 const context = canvas.current.getContext('2d');
-                const data = context.getImageData(0, 0, canvas.current.width, canvas.current.height);
+                // const data = context.getImageData(0, 0, canvas.current.width, canvas.current.height);
 
                 if (!model) {
-                    setModel(await loadMobileNet());
+                    // setModel(await loadMobileNet());
+                    setModel(await newModel());
                 } else {
-                    setPredictions(await classifyImage(model, data));
+                    // setPredictions(await classifyImage(model, data));
+                    setPredictions(await predictWithNewModel(model, canvas.current));
                 }
             };
 
@@ -44,6 +47,11 @@ export default function Predictions(props) {
     return (
         <div className={predictionClasses}>
             <h2>Predictions</h2>
+
+            {canvas.current &&
+                <a style={{ color: "#000000" }} download="test.png" href={canvas.current.toDataURL('image/png').replace('image/png', 'image/octet-stream')}>test</a>
+            }
+
             <table className="predictions__table">
                 <thead>
                     <tr>
